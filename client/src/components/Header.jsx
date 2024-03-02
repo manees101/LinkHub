cb5e9053bad4f94965cbdac176f0a5cc5d3b49b2
Muhammad Anees <muhammadaneesrajpoot@gmail.com>
2024-03-02 12:07:46 +0500

    first commit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import appLogo from "../assets/images/logo.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
-
+import {useNavigate} from "react-router-dom"
 function Header() {
+  const navigate=useNavigate();
   const searchHandler=()=>{
 
   }
   return (
     <div className='flex'>
-      <img src={appLogo} alt="logo" className='h-10 w-11 mt-3 ' />
+      <img src={appLogo} alt="logo" className='h-9 w-10 mt-3 cursor-pointer' onClick={()=>navigate('/home')} 
+      
+      />
       <div className=' w-54 h-8  flex m-4 '>
         <input type="text" placeholder='#Explore' className=' ml-1 p-2' onChange={searchHandler}/>
         <div className='w-8 h-8 bg-amber-400 rounded-sm flex justify-center items-center'>
